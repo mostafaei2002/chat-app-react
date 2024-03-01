8603db259f60a36d700f73aa7c3101ad8a147964
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-03-01 16:54:22 +0330

    change active user bg

--- src/features/users/User.jsx
+++ src/features/users/User.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
 function User({ user }) {
-  return <li className="hover:bg-slate-500 py-2 px-4 ">{user.name}</li>;
+  const activeUser = useSelector((state) => state.users.currentUser);
+  console.log(user, activeUser);
+  const isActive = activeUser === user.name;
+
+  return (
+    <li className={`py-2 px-4 ${isActive ? "bg-slate-300" : ""} `}>
+      {user.name}
+    </li>
+  );
 }
 
 export default User;
--- src/features/users/Users.jsx
+++ src/features/users/Users.jsx
@@ -3,7 +3,6 @@ import User from "./User";
 
 function Users() {
   const users = useSelector((state) => state.users.users);
-  console.log(users);
 
   return (
     <ul className="row-span-2 bg-slate-100 border-r-2 border-r-slate-200 divide-y divide-slate-200">
